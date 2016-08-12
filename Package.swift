bcb895fe71239ab8c57ae9be75164071b0fa0f4b
noppoman <yuki@miketokyo.com>
2016-08-12 14:13:18 +0900

    use open-swift/S4

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
 	dependencies: [
         .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/slimane-swift/URI.git", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/noppoMan/S4.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 12),
     ]
 )
